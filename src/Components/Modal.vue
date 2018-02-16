b0fe9deecd974869e551ff6343ceebcc82174e92
piela.bartek@gmail.com <piela.bartek@gmail.com>
2018-02-16 16:35:02 +0100

    some style

--- src/Components/Modal.vue
+++ src/Components/Modal.vue
@@ -4,18 +4,23 @@
 			<div class="modal-wrapper">
 				<div class="modal-container">
 					<div class="modal-header">
-						<h3>New Note</h3>
+						<input class="modal-input" type="text" placeholder="Note Title" v-model="note.title">
 					</div>
 					<div class="modal-body">
-						<input type="text" placeholder="Note title" v-model="note.title">
-						<textarea name="" id="" cols="30" rows="10" v-model="note.description"></textarea>
-						<select>
-							<option v-for="category in categories" :value="category.value">{{category.name}}</option>
-						</select>
+						<textarea class="modal-textarea " name="" id="" cols="30" rows="10" v-model="note.description"
+						          placeholder="body"></textarea>
+
+						<div class="modal-category">
+							<span class="mr-s">Category:</span>
+							<select class="modal-select" v-model="note.category">
+								<option v-for="category in categories" :value="category.value">{{category.name}}
+								</option>
+							</select>
+						</div>
 					</div>
 					<div class="modal-footer">
-						<button @click="$emit('close')">Close</button>
-						<button @click="addNote(); $emit('close')">Add</button>
+						<a class="btn btn-text-red" @click="$emit('close')">Close</a>
+						<a class="btn btn-text-green" @click="addNote(); $emit('close')">Add</a>
 					</div>
 				</div>
 			</div>
@@ -32,12 +37,13 @@
 					date: '',
 					title: '',
 					description: '',
-					category: ''
+					category: category.PROJECTS
 				},
 				categories: [
 					{
 						value: category.PROJECTS,
 						name: 'Projects',
+						selected: true
 
 					},
 					{
